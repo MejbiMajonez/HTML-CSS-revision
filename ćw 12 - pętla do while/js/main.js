a5140f5836cfc2ed22f515335bfa9198c4f91efe
Simon <38011360+MejbiMajonez@users.noreply.github.com>
2018-06-08 01:21:53 +0200

    s

--- ćw 12 - pętla do while/js/main.js
+++ ćw 12 - pętla do while/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-p2();
+p2b();
 });
-function p2(){
-  i=50;
-  while (i>0){
-    $('#box2').append('<br>'+i);
-    i -=1;
-  }
+function p2b(){
+  i=50;-50
+  do {
+    $('#box3').append('<br>'+i);
+    i -=10;
+  }while(i>0);
 }
