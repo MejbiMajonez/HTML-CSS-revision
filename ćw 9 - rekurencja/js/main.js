77c075820d781bfbf0d294ad11333ea321d81dc3
Simon <38011360+MejbiMajonez@users.noreply.github.com>
2018-06-08 01:07:57 +0200

    Funkcja rekurencyjna

--- ćw 9 - rekurencja/js/main.js
+++ ćw 9 - rekurencja/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-odliczanko(10);
+odliczanko(11);
 });
-function odliczanko(10);
+function odliczanko(licznik){
+  licznik--;
+  if(licznik>0){
+    $('#box_informacyjny').append(licznik+"<br>");
+    setTimeout(function(){
+      odliczanko(licznik);
+    },1000);
+  }else{
+    $('box_informacyjny').append("Nowy Rok");
+  }
+}
