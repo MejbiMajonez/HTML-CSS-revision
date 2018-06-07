390185c3c122dd3ad46ca11150b8eeaf2b25086c
Simon <38011360+MejbiMajonez@users.noreply.github.com>
2018-06-07 15:58:06 +0200

    no message

---
+++ ćw 1 - rekurencja/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+wypis2();
+});
+function wypis2(){
+ var jajko = "Stefan"
+ var kutaz = "John"
+ var jajebe = "39"
+
+  $("#imie").html(jajko);
+  $("#imie2").html(kutaz);
+  $('.wiek').html(jajebe);
+
+}
