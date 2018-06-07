d62885305d7816237a3c113ef711fe5f3e33594c
Simon <38011360+MejbiMajonez@users.noreply.github.com>
2018-06-07 23:48:11 +0200

    no message

--- ćw 6 - zmienne lokalne i globalne/js/main.js
+++ ćw 6 - zmienne lokalne i globalne/js/main.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
 var zmienna_globalna = 0;
 
 function go_function1(){
-var  zmienna_lokalna= 0;
-var zmienna_globalna++;
-  
+ var zmienna_lokalna= 0;
+ zmienna_globalna++
+  console.log('zmienna_globalna: '+zmienna_globalna+',zmienna_lokalna '+zmienna_lokalna);
+}
+function go_function2(){
+  zmienna_lokalna= 7;
+  zmienna_globalna++
+  console.log('zmienna_globalna: '+zmienna_globalna+',zmienna_lokalna '+zmienna_lokalna);
 }
