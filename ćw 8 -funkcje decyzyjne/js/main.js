64b38974d3d0e515e0cf9a5f29433abc91bcc743
Simon <38011360+MejbiMajonez@users.noreply.github.com>
2018-06-08 00:33:54 +0200

    no message

--- ćw 8 -funkcje decyzyjne/js/main.js
+++ ćw 8 -funkcje decyzyjne/js/main.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
 
 });
-function zmienMnie(){
-  wartosc = $('#wartosc').val();
-  wartosc2 = zamien_na_hex(wartosc);
-  $('#wartosc2').val(wartosc2);
-}
-function zamien_na_hex(wartosc){
-  switch(wartosc){
-    case '10': text ='A'; break;
-    case '11': text ='B'; break;
-    case '12': text ='C'; break;
-    case '13': text ='D'; break;
-    case '14': text ='E'; break;
-    case '15': text ='F'; break;
-    default:text = wartosc;
-
-  }
-  return text;
+x=5;y=7;
+if(x>=4 && y<10){
+  console.log("prawda");
+}else{
+  console.log("fałsz");
 }
