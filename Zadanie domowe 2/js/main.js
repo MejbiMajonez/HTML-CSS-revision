2865e8205c993fb1c9e27d95fbd94c328308342a
Simon <38011360+MejbiMajonez@users.noreply.github.com>
2018-06-08 21:09:50 +0200

    l

--- Zadanie domowe 2/js/main.js
+++ Zadanie domowe 2/js/main.js
@@ -7,7 +7,8 @@ function go_kwadracik() {
   grafika();
   rusz_mnie();
   boom();
-  takise();
+  zmien_size();
+  szybciej();
 }
 //grafika na starcie
 function grafika(){
@@ -21,15 +22,16 @@ function rusz_mnie() {
 
     var key = event.which;
     var peniz = $('#kwadrat');
-
+    var Szwynia ="-=1"
+    var Wiepsz ="+=1"
     if(key == 37 && parseInt(peniz.css('left')) > 0){
-      peniz.css('left', '-=1');
+      peniz.css('left', Szwynia);
     }else if(key == 38 && parseInt(peniz.css('top')) > 0){
-      peniz.css('top', '-=1');
+      peniz.css('top', Szwynia);
     }else if(key == 39 && parseInt(peniz.css('left')) < 788){
-      peniz.css('left', '+=1');
+      peniz.css('left', Wiepsz);
     }else if(key == 40 && parseInt(peniz.css('top')) < 388){
-      peniz.css('top', '+=1');
+      peniz.css('top', Wiepsz);
     }
 
   });
@@ -60,6 +62,7 @@ function takise() {
   }
 });
 }
+//input z logiem geekfactory
 function odczyt(){
   fjut = $('#Geek').val();
   if(fjut == "Geek"){
@@ -67,4 +70,32 @@ $("#container").addClass("GF");
 }else{
   alert('Nie tym razem, Danielu, pisz to Geek!')
 }
+}
+// zmiana rozmiaru
+function zmien_size(){
+  $(document).on('keydown', function(){
+    let key = event.which;
+    if(key == 67){
+      $('#kwadrat').toggleClass('kwadracik duzy_kwadrat');
+    }
+  });
+}
+//przyśpieszenie
+function szybciej() {
+  $(document).on('keydown', function (event) {
+
+    var key = event.which;
+    var dupsko = $('#kwadrat');
+
+    if(key == 18 && parseInt(dupsko.css('left')) > 0){
+      dupsko.css('left', '-=5px');
+    }else if(key == 18 && parseInt(dupsko.css('top')) > 0){
+      dupsko.css('top', '-=5px');
+    }else if(key == 18 && parseInt(dupsko.css('left')) < 788){
+      dupsko.css('left', '+=5px');
+    }else if(key == 18 && parseInt(dupsko.css('top')) < 388){
+      dupsko.css('top', '+=5px');
+    }
+
+  });
 }
